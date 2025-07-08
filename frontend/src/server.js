fa4c68d6f8ddbdbf897adce83f92cab496204f5e
Midhat-dot <midhatcodes@gmail.com>
2025-07-07 20:30:26 -0700

    updated

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,2 +1,1 @@
-export const server = "https://multivendor-151v-hsw7ygu8u-midhat786s-projects.vercel.app/api/v2";
-export const backend_url = "https://multivendor-151v-hsw7ygu8u-midhat786s-projects.vercel.app";
+export const server = "https://multivendor-151v-hsw7ygu8u-midhat786s-projects.vercel.app";
