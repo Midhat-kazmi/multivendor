233cc3c550096220d792f59f987c23824358d341
Midhat-dot <midhatcodes@gmail.com>
2025-07-11 00:41:15 -0700

    update

--- backend/app.js
+++ backend/app.js
@@ -13,10 +13,12 @@ const path = require("path");
 app.use(
   cors({
     origin: "https://multivendor-five.vercel.app",
-    
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
 
 
 // Middleware to parse JSON and URL encoded data
