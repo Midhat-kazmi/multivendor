a88f8ce28c606cc1a9ad7a441d3653ada1d4478a
Midhat-dot <midhatcodes@gmail.com>
2025-07-10 17:24:34 -0700

    sendtoken updated

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,21 +1,22 @@
-//create token and saving in cookies
+// Create token and save in cookies
 const sendToken = (user, statusCode, res) => {
-    const token = user.getJwtToken(); // Generate JWT token
-    
-    // Options for cookie
-    const options = {
-        expires: new Date(
-        Date.now() + process.env.COOKIE_EXPIRES * 24 * 60 * 60 * 1000
-        ),
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production" ? true : false,
-    };
-    
-    res.status(statusCode).cookie("token", token, options).json({
-        success: true,
-        token,
-        user,
-    });
-}
+  const token = user.getJwtToken(); // Generate JWT token
+
+  // Options for cookie
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.COOKIE_EXPIRES * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production", // true only in production
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // important for cross-origin cookies
+  };
+
+  res.status(statusCode).cookie("token", token, options).json({
+    success: true,
+    token,
+    user,
+  });
+};
 
 module.exports = sendToken;
