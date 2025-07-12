6de75ca1d5b17982d8b7e7ad13a36ac69ef7cad7
Midhat-dot <midhatcodes@gmail.com>
2025-07-12 09:27:49 -0700

    fix

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -15,9 +15,14 @@ const Hero = () => {
         <h1
           className={`text-[35px] leading-[1.2] 800px:text-[60px] text-[white] font-[600] capitalize`}
         >
-          Find Your Vibe!
+          Find Your Vibe
         </h1>
-      
+        <p className="pt-5 text-[16px] font-[Poppins] font-[400] text-[]">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae,
+          assumenda? Quisquam itaque <br /> exercitationem labore vel, dolore
+          quidem asperiores, laudantium temporibus soluta optio consequatur{" "}
+          <br /> aliquam deserunt officia. Dolorum saepe nulla provident.
+        </p>
         <Link to="/products" className="inline-block">
             <div className={`${styles.button} mt-5`}>
                  <span className="text-[#fff] font-[Poppins] text-[18px]">
